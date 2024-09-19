50a784e40e240dd74e3729c4e5f6219cbd291ab7
kevin <kevin@XDXDXD>
2024-09-19 11:18:19 -0300

    Adicionar arquivos de projeto.

---
+++ dawadawdadwdawdawawdawdawdawdawdawdawdadawdawdawd/Pages/Components/Livro.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace teste2354.Pages.Components
+{
+public class LivroModel : PageModel
+  {
+  public void OnGet()
+ {
+  }
+ }
+}
---
+++ dawadawdadwdawdawawdawdawdawdawdawdawdadawdawdawd/Pages/LivroModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeuNamespace.Pages
+{
+ public class LivroModel : PageModel
+    {
+        public int Id { get; set; }
+ public string? Titulo { get; set; } 
+ public string? Autor { get; set; } 
+ public void OnGet(int id)
+  {
+  Id = id;
+   Titulo = "Jujutsu Kaisen";
+   Autor = "Gege Akutami";
+   }
+ }
+}
